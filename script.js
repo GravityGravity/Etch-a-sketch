fd1295d2f28e4a8bd9f402eb8df30faabb90640a
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-09-16 18:33:08 -0400

    Draft sketch container cell division + changes...
    refactor: move sketch options into main for consistent UI placement
    feat: add initial cell division simulation
    fix: limit sketch container width to sketch height to prevent container
    from reaching out of window viewport

---
+++ script.js
@@ -0,0 +1,20 @@
+// Globals
+
+
+// Mutable Vars
+let gridSize = 16; //cells per row
+
+// Queries
+const sketchContainer = document.querySelector('#container');
+
+// Functions
+function partitionContainer (width, height) {
+
+    
+
+
+    return sizePerBox;
+}
+
+// Code
+partitionContainer(sketchContainer.clientWidth, sketchContainer.clientHeight);

831240ff77706e338835b968cd5d5f0d434a62c7
Grav <154651849+GravityGravity@users.noreply.github.com>
2026-09-16 20:08:11 -0400

    feat: Add sketch grid creation
    Add grid creation in JS on application startup.

--- script.js
+++ script.js
@@ -2,19 +2,33 @@
 
 
 // Mutable Vars
-let gridSize = 16; //cells per row
+let gridSize = 16; //cells per row and per column
 
 // Queries
 const sketchContainer = document.querySelector('#container');
-
 // Functions
-function partitionContainer (width, height) {
 
-    
+function createRow () {
+    const row = document.createElement('div');
+    row.classList.add('row');
 
+    for (let i = 0; i < gridSize; i++){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.append(cell);
+    }
 
-    return sizePerBox;
+    return row;
+}
+
+function createSketchGrid () {
+
+    for (let i = 0; i < gridSize; i++) {
+        sketchContainer.appendChild(
+            createRow()
+        );
+    }
 }
 
 // Code
-partitionContainer(sketchContainer.clientWidth, sketchContainer.clientHeight);
+if (!sketchContainer.hasChildNodes()) createSketchGrid();
